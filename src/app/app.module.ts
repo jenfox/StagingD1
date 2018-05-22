f9db929edbfb390810d624e02737a81b3ccc8f38
Jennifer Fox <jrufox@gmail.com>
2018-05-22 10:13:15 -0400

    project start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-
+// Register modules and dependencies here
+// they're loaded when app is built/loaded
+// a module loads everything inside of it at same time
+// with multiple modules you can defer some loading
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
